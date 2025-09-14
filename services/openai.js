bf3e7930e8aebe2fa909106e118b4601c1a589a8
Sumit Kumar <sumitkumar993618@gmail.com>
2025-09-15 01:49:44 +0530

    fix: worked on gemini compaitibility

--- services/openai.js
+++ services/openai.js
@@ -1,13 +1,16 @@
 const OpenAI = require("openai");
 require("dotenv/config");
 
-const client = OpenAI();
+const client = OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
 
 const chatCompletion = async(userInput, model) =>{
     console.log(model)
     const response = await client.chat.completions.create(
         {
-            model: model,
+            model: "gemini-2.0-flash",
             messages: [
                 { role: 'user', content: userInput },
             ]
@@ -16,5 +19,21 @@ const chatCompletion = async(userInput, model) =>{
 
     return response.choices[0].message.content
 }
+
+// const client = OpenAI();
+
+// const chatCompletion = async(userInput, model) =>{
+//     console.log(model)
+//     const response = await client.chat.completions.create(
+//         {
+//             model: model,
+//             messages: [
+//                 { role: 'user', content: userInput },
+//             ]
+//         }
+//     );
+
+//     return response.choices[0].message.content
+// }
 
 module.exports = {chatCompletion}
